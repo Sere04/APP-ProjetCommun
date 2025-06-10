335771008a4d1ad924dd94221770fcc31415b5f7
Maxlrm <mleroymeunier.mlrm@gmail.com>
2025-06-10 18:56:30 +0200

    Correction du design de la page d'accueil

---
+++ app/Views/components/footer.js
@@ -0,0 +1,23 @@
+export function renderFooter() {
+    return `
+    <link rel="stylesheet" href="/app/Views/components/footer.css">
+    <footer class="main-footer glass-effect">
+        <div class="footer-logo">
+            <img src="/app/Views/Assets/img/logo.png" alt="Icône MakerHUB">
+        </div>
+        <div class="footer-infos">
+            <p>MakerHUB</p>
+            <p>© 2025, <a href="#" class="underline">GNU v3.0</a></p>
+        </div>
+        <ul class="footer-links">
+            <li><a href="#">Nous contacter</a></li>
+            <li><a href="#">Mentions légales</a></li>
+            <li><a href="#">Cookies</a></li>
+            <li><a href="#">CGU</a></li>
+            <li><a href="#">CGPS</a></li>
+            <li><a href="#">FAQ</a></li>
+        </ul>
+    </footer>
+    <script src="https://kit.fontawesome.com/c54f418805.js" crossorigin="anonymous"></script>
+    `;
+}
