0f0e9ed28b8a0146d882341824d081f6734c1006
Maxlrm <mleroymeunier.mlrm@gmail.com>
2025-06-10 15:22:45 +0200

    Page d'accueil

---
+++ app/Views/home.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="Layouts/home.css">
+</head>
+<body>
+    <div class="background-circle"></div>
+    <div class="blurred-overlay">
+        <div class="content">
+            <h1>Bienvenue sur la page d'accueil</h1>
+            <p>Effet de profondeur avec dégradé et flou.</p>
+        </div>
+    </div>
+</body>
+</html>
